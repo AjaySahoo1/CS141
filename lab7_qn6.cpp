5799b1a66d8c29a2e7db58593eb79bf2be75f4de
AjaySahoo1 <42172797+AjaySahoo1@users.noreply.github.com>
2018-10-07 02:55:57 +0530

    Update lab7_qn6.cpp

--- lab7_qn6.cpp
+++ lab7_qn6.cpp
@@ -1,37 +1,39 @@
 /******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+ Write a C++ program to find reverse of any number using recursion.
 
 *******************************************************************************/
 
 #include <iostream>
 using namespace std;
-
+//Define function
 int reverse(int num) 
 {
-    static int rem,sum;
-    if(num>0)
-    {
-      rem = num%10;
-      sum = sum*10 + rem;
-      /* function call itself. */
-      reverse(num/10);
-    }
-    else
-    {
-        return sum;
-    }
+  //Declare static variables
+  static int rem,sum;
+  ..Put condition for recursion and its turnmination
+  if(num>0)
+  {
+    rem = num%10;
+    sum = sum*10 + rem;
+    /* function call itself. */
+    reverse(num/10);
+  }
+  else
+  {
+    return sum;
+  }
 }
+//Main Function
 int main()
 {
-    int num,rev;
-    /* Taking input. */
-    cout<<"Enter number:";
-    cin >> num;
-    /* Called reverse function .*/
-    rev = reverse(num);
-    cout << "Reverse of a input number is:" << rev;
-    return 0;
+  Declare variables
+  int num,rev;
+  //Taking input
+  cout<<"Enter number:";
+  cin >> num;
+  //Called reverse function
+  rev = reverse(num);
+  //Printing the reversed value
+  cout << "Reverse of a input number is:" << rev;
+  return 0;
 }
