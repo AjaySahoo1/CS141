6d9277d1f2fbe513c066eae06f5ceae3d5bacec2
AjaySahoo1 <42172797+AjaySahoo1@users.noreply.github.com>
2018-10-06 15:28:18 -0400

    Add files via upload

---
+++ lab7_qn7.cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+ Write a C++ program to check whether a number is palindrome or not using recursion.
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+//Define a function
+int pal(int num) 
+{
+    //Use static variable so that it won't change during recursion
+    static int rev,sum;
+    //Put condition to find the reverse
+    if(num>0)
+    {
+      rev = num%10;
+      sum = sum*10 + rev;
+      //Recursion
+      pal(num/10);
+    }
+    //Ending the recursion
+    else
+    {
+        return sum;
+    }
+}
+//Main function
+int main()
+{
+    int num,rev;
+    //Take input
+    cout<<"Enter number:";
+    cin >> num;
+    //Called function
+    rev = pal(num);
+    //Put condition to check it is pallindrome or not
+    if(rev==num)
+    cout<<"It is a pallindrome number";
+    else
+    cout<<"It is not a pallindrome number";
+    return 0;
+}
