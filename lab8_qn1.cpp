576198bac09a918312915fc2d04c48a3b0e93773
AjaySahoo1 <42172797+AjaySahoo1@users.noreply.github.com>
2018-10-11 10:08:39 -0400

    Add files via upload

---
+++ lab8_qn1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+//Define function with proper parameters
+int arr(int ar[], int size)
+{
+	int i;
+	int sum=0;
+//Use loop to find the sum of all elements present in the array
+	for(i=0;i<size;i++)
+	{
+		sum=sum+ar[i];
+	}
+//Print the sum
+	cout<<"Sum of all elements in array:"<<sum<<endl;
+}
+//Main function
+int main()
+{
+//Ask user to put the size of the array
+	int size;
+	cout<<"Enter size"<<endl;
+	cin>>size;
+	int ar[size];
+//Ask user to input values to the array
+	cout<<"Put input to array"<<endl;
+//Use loop to input the values
+	for(int i=0;i<size;i++)
+	{
+		cin>>ar[i];
+	}
+//Call the function
+	arr(ar,size);
+	return 0;
+}
