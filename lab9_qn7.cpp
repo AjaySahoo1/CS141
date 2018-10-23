ed98493802417fc9fbfefd8323e366338c8f05ea
AjaySahoo1 <42172797+AjaySahoo1@users.noreply.github.com>
2018-10-23 18:40:38 +0530

    Update lab9_qn7.cpp

--- lab9_qn7.cpp
+++ lab9_qn7.cpp
@@ -1,46 +1,43 @@
+/*Write a function that returns a pointer to the maximum value of an array of double's. 
+If the array is empty, return NULL.*/
 #include <iostream>
 using namespace std;
 double* maximum(double* a, int size)
 {
-    if(size!=0)
+   
+    int x;
+    //sort array in descending order
+    for(int i=0;i<size;i++)
     {
-        int x;
-        //sort array in descending order
-	    for(int i=0;i<size;i++)
+	    for(int j=i+1;j<size;j++)
 	    {
-		    for(int j=i+1;j<size;j++)
+		    if(*(a+i)<*(a+j))
 		    {
-			    if(*(a+i)<*(a+j))
-			    {
-				    x=*(a+i);
-				    *(a+i)=*(a+j);
-				    *(a+j)=x;
-			    }
+			    x=*(a+i);
+			    *(a+i)=*(a+j);
+			    *(a+j)=x;
 		    }
 	    }
-	    return a;
     }
-    else
-    {
-        return NULL;
-    }
+    return a;
 }
 int main()
 {
+    //Give the size of the array and put input
     int size;
     cout<<"Enter size of the array"<<endl;
     cin>>size;
     
     double ar[size];
-    cout<<"Enter input to array"<<endl;
-    for(int i=0;i<size;i++)
+    if(size!=0)//Check whether the size is zero or not
     {
-        cin>>ar[i];
-    }
-    if(size!=0)
-    {
+        cout<<"Enter input(charcter by character)to array"<<endl;
+        for(int i=0;i<size;i++)
+        {
+            cin>>ar[i];
+        }
         double* p=&ar[0];
-        cout<<"Max:"<<*maximum(p,size);
+        cout<<"Max:"<<*maximum(p,size);//call the function
     }
     else
     {
