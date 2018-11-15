1f3bf7b3d0295d4af4809cfad9227bb01b2082d3
AjaySahoo1 <42172797+AjaySahoo1@users.noreply.github.com>
2018-11-15 16:04:30 +0530

    Update lab9_qn17_vi.cpp

--- lab9_qn17_vi.cpp
+++ lab9_qn17_vi.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-// returns true if X and Y are same
+// returns true if str1 and str2 are same
 int compare(const char *str1, const char *str2)
 {
 	while (*str1 && *str2)
